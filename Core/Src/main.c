2a77b3736232746d030b1f525665823cd4a0dac1
Marc Freudenberg <freudenbergmarc@gmail.com>
2021-02-11 23:01:23 +0100

    Configure filter for freq meas

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -487,7 +487,7 @@ static void MX_TIM3_Init(void)
   sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
   sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
   sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
-  sConfigIC.ICFilter = 3;
+  sConfigIC.ICFilter = 15;
   if (HAL_TIM_IC_ConfigChannel(&htim3, &sConfigIC, TIM_CHANNEL_2) != HAL_OK)
   {
     Error_Handler();
